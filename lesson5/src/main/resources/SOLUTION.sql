956c9c9e1852e3350e977e96f7fb8fbe2c2029f1
Davidchanz <tovliskatsi@gmail.com>
2023-08-04 17:08:11 +0400

    SQL Task 1 Lesson 5 fix

--- lesson5/src/main/resources/SOLUTION.sql
+++ lesson5/src/main/resources/SOLUTION.sql
@@ -6,11 +6,11 @@ SELECT * FROM Student WHERE birthday >= DATEADD(YEAR, -20, CURRENT_DATE()) AND g
 
 SELECT * FROM Student WHERE name = 'Mike' OR (groupnumber >= 4 AND groupnumber <= 6);
 
-SELECT * FROM Payment WHERE payment_date > DATEADD(MONTH, -8, CURRENT_DATE());
+SELECT * FROM Payment WHERE payment_date > DATEADD(MONTH, -8, '20230101');
 
 SELECT * FROM Student WHERE name LIKE 'A%';
 
-SELECT * FROM Student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9)
+SELECT * FROM Student WHERE (name = 'Roxi' AND groupnumber = 4) OR (name = 'Tallie' AND groupnumber = 9);
 
 
 
